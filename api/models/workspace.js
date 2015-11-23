1c2d0fe5c196cb2c38f82e98947a3413b29c93e5
sareh <sareh.heidari@gmail.com>
2015-11-23 14:04:09 +0000

    Changed optional and compulsory fields

--- api/models/workspace.js
+++ api/models/workspace.js
@@ -10,15 +10,15 @@ var workspaceSchema = new mongoose.Schema({
     contactName: { type: String },
     twitter:     { type: String, required: true },
     facebook:    { type: String },
-    openingTimesWeekdayOpen:  { type: String, required: true },
-    openingTimesWeekdayClose: { type: String, required: true },
-    openingTimesWeekendOpen:  { type: String, required: true },
-    openingTimesWeekendClose: { type: String, required: true },
-    photo:       { type: String },
-    averageRent: { type: String, required: true },
-    services:    { type: String, required: true },
+    openingTimesWeekdayOpen:  { type: String },
+    openingTimesWeekdayClose: { type: String },
+    openingTimesWeekendOpen:  { type: String },
+    openingTimesWeekendClose: { type: String },
+    photo:                    { type: String },
+    averageRent:              { type: String },
+    services:                 { type: String },
     sector:      { type: String, required: true },
-    type:        { type: String, required: true }
+    spaceType:                { type: String}
 });
 
 module.exports = mongoose.model("Workspace", workspaceSchema);
