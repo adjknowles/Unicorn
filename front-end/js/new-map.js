79a378faf81f19b636bd6a72f092581d22d2c82f
sareh <sareh.heidari@gmail.com>
2015-11-25 12:13:35 +0000

    Centering on place

--- front-end/js/new-map.js
+++ front-end/js/new-map.js
@@ -20,6 +20,14 @@ mapApp.captureLatLng = function(){
   $("#new-startup-name").val(mapApp.newStartup.name);
 }
 
+mapApp.addMarker = function(){
+  
+}
+
+mapApp.removeAllMarkers = function(){
+
+}
+
 mapApp.postStartup = function(){
   event.preventDefault();
 
@@ -64,7 +72,7 @@ mapApp.init = function(){
     center: center,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(canvas, mapOptions);
+  this.map = new google.maps.Map(canvas, mapOptions);
 
   // Top SearchBox
   var searchBox = new google.maps.places.Autocomplete(document.getElementById('searchbox'));
@@ -74,14 +82,27 @@ mapApp.init = function(){
     console.log(mapApp.place);
   });
   google.maps.event.addDomListener(window, 'resize', function(){
-    map.setCenter(center);
+    mapApp.map.setCenter(center);
   });
 
   // Add new search bar
   var placeSearchBox = new google.maps.places.Autocomplete(document.getElementById('place-search-box'));
   google.maps.event.addListener(placeSearchBox, 'place_changed', function(){
+
     mapApp.newStartup = placeSearchBox.getPlace();
     // Add either zoom in on place, or add marker or whatever
+    
+    var icon = 'http://orig11.deviantart.net/cb61/f/2010/190/d/b/rainbow_unicorn_icon_by_unicornmolester.gif';
+
+    var marker = new google.maps.Marker({
+      map: mapApp.map,
+      icon: icon,
+      title: mapApp.newStartup.name,
+      position: mapApp.newStartup.geometry.location
+    });
+
+    // Recenter the map onto the newStartup position
+    mapApp.map.setCenter(mapApp.newStartup.geometry.location);
     console.log(mapApp.newStartup);
   });
 
