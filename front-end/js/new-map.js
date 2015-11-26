d6f4c181babf5bcf98fb641588a84f03b7e7b211
adjknowles <alastair_knowles@yahoo.co.uk>
2015-11-26 10:03:21 +0000

    Changed styling of map

--- front-end/js/new-map.js
+++ front-end/js/new-map.js
@@ -107,10 +107,122 @@ mapApp.postPlace = function(){
 
 mapApp.init = function(){
 
+  var mapStyle = [
+  {
+    "featureType": "administrative",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#444444"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "labels.icon",
+    "stylers": [
+    {
+      "saturation": "15"
+    },
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.icon",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+    {
+      "color": "#f2f2f2"
+    }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+    {
+      "saturation": -100
+    },
+    {
+      "lightness": 45
+    }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [
+    {
+      "visibility": "simplified"
+    }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.icon",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+    {
+      "color": "#13B9DF"
+    },
+    {
+      "visibility": "on"
+    }
+    ]
+  }
+  ];
+
   mapApp.canvas = document.getElementById('googleMap');
   mapApp.center = new google.maps.LatLng(51.507904, -0.127466)
   mapApp.mapOptions = {
     zoom: 13,
+    styles: mapStyle,
     center: mapApp.center,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
