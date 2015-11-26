30d2581e082f3b55dd811a902a1c8ad8f908b85c
sedstan <sedky.stanley.said@gmail.com>
2015-11-26 17:48:27 +0000

    udated footer and added twitter share button.

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -174,3 +174,10 @@ function displayErrors(data){
 function hideErrors(){
   return $(".alert").removeClass("show").addClass("hide");
 }
+
+// Twitter share button
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+// Facebook share button
+
+
